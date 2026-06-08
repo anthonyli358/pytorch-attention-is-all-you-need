f4c60c6883c3ab9c02c7bd4e9c00054a10c4d3af
anthonyli358 <anthonyli358@gmail.com>
2026-06-08 18:08:30 +0100

    encoder n=6

--- src/encoder.py
+++ src/encoder.py
@@ -1,7 +1,26 @@
 import torch
 from torch import nn
+from embedder import Embedder
 from src.multi_head_attention import MultiHeadAttention
 from src.positionwise_feedforward import PositionwiseFeedForward
+
+
+class Encoder(nn.Module):
+    def __init__(
+        self, vocab_size, n_layers=6, d_model=512, d_ff=2048, n_heads=8, dropout=0.1
+    ):
+        super().__init__()
+        self.embedding = Embedder(vocab_size, d_model)
+        self.layers = nn.ModuleList(
+            [EncoderLayer(d_model, d_ff, n_heads, dropout) for _ in range(n_layers)]
+        )
+
+    def forward(self, tokens, mask=None):
+        x = self.embedding(tokens)
+        for layer in self.layers:
+            x = layer(x, mask)
+        return x
+
 
 class EncoderLayer(nn.Module):
     def __init__(self, d_model=512, d_ff=2048, n_heads=8, dropout=0.1):
@@ -19,12 +38,9 @@ class EncoderLayer(nn.Module):
         x = self.attention(x, x, x, mask)
         x = self.dropout1(x)
         x = self.norm1(_x + x)
-
         # ffwd + (add + norm)
         _x = x
         x = self.ffn(x)
         x = self.dropout2(x)
         x = self.norm2(_x + x)
-
         return x
-
